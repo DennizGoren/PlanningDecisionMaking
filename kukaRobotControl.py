3f3c526761c17e065a2bd1d3453969527307bbad
adenw <adenwestmaas@hotmail.com>
2023-01-12 11:14:53 +0100

    latest version

--- kukaRobotControl.py
+++ kukaRobotControl.py
@@ -10,18 +10,18 @@ import time
 from tqdm import tqdm
 from scipy import interpolate
 
+
 class RobotControl:
     def __init__(self):
-        self.start = [3, 11]
-        self.goal = [17, 4]
+        self.start = [0, 0]
+        self.goal = [15, 11]
         self.robot_radius = 0.8
         self.client = RemoteAPIClient()
         self.client.setStepping(True)
         self.sim = self.client.getObject('sim')
         self.idle_fps = self.sim.getInt32Param(self.sim.intparam_idle_fps)
-        self.robot_object = self.sim.getObject("/youBot")
+        self.robot_object = self.sim.getObject("/LineTracer")
         self.world_frame = self.sim.handle_world
-        # self.get_circle_obstacles = self.get_circle_obstacles()
 
         # get all racks from Coppeliasim scene
         self.racks = []
@@ -34,28 +34,18 @@ class RobotControl:
                 break
 
         # initialize wheel joints
-        self.front_left_wheel_joint = self.sim.getObject('./rollingJoint_fl')
-        self.rear_left_wheel_joint = self.sim.getObject('./rollingJoint_rl')
-        self.rear_right_wheel_joint = self.sim.getObject('./rollingJoint_rr')
-        self.front_right_wheel_joint = self.sim.getObject('./rollingJoint_fr')
-
-        # initialize robot arm joints
-        self.joint_0 = self.sim.getObject('./youBotArmJoint0')
-        self.joint_1 = self.sim.getObject('./youBotArmJoint1')
-        self.joint_2 = self.sim.getObject('./youBotArmJoint2')
-        self.joint_3 = self.sim.getObject('./youBotArmJoint3')
-        self.joint_4 = self.sim.getObject('./youBotArmJoint4')
-        self.gripper_joint_1 = self.sim.getObject('./youBotGripperJoint1')
-        self.gripper_joint_2 = self.sim.getObject('./youBotGripperJoint2')
+        self.left_wheel_joint = self.sim.getObject('./DynamicLeftJoint')
+        self.right_wheel_joint = self.sim.getObject('./DynamicRightJoint')
 
     def get_circle_obstacles(self):
         obstacle_list = []
         radius = 0.16711
-        for rack in self.racks:
+        for i, rack in enumerate(self.racks):
             x_rack, y_rack, _ = self.getObjectPosition(rack)
+            theta = self.getObjectOrientation(rack)[2]
             y_start = y_rack + 1.055
             for i in range(11):
-                obstacle = (x_rack, (y_start - 0.211*i), radius)
+                obstacle = (x_rack, (y_start - 0.211 * i), radius)
                 obstacle_list.append(obstacle)
         return obstacle_list
 
@@ -64,62 +54,60 @@ class RobotControl:
             algo = RRTStar(
                 start=self.start,
                 goal=self.goal,
-                max_iter=600,
-                rand_area=[0, 20],
+                max_iter=1000,
+                rand_area=[-10, 20],
                 obstacle_list=self.get_circle_obstacles(),
-                expand_dis=1,
-                search_until_max_iter= True,
+                expand_dis=3,
+                search_until_max_iter=True,
                 robot_radius=self.robot_radius)
 
         else:
             algo = RRT(
                 start=self.start,
                 goal=self.goal,
-                rand_area=[0, 20],
+                rand_area=[-10, 20],
                 obstacle_list=self.get_circle_obstacles(),
                 play_area=[0, 20, 0, 20],
                 robot_radius=self.robot_radius)
         tic = time.perf_counter()
-        path = algo.planning(animation=False) # this path is the reference trajectory for the PID
+        path = algo.planning(animation=True)  # this path is the reference trajectory for the PID
         toc = time.perf_counter()
         print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
 
-        print(path)
-        
+        # print(path)
 
         if path is None:
             print("Cannot find path")
         else:
             print("found path!!")
             # Path smoothing
-            maxIter = 2000
-            smoothedPath = algo.path_smoothing(path, maxIter, self.get_circle_obstacles())
+            # maxIter = 2000
+            # smoothedPath = algo.path_smoothing(path, maxIter, self.get_circle_obstacles())
             algo.draw_graph()
             plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
-            plt.plot([x for (x, y) in smoothedPath], [y for (x, y) in smoothedPath], '-c')
+            # plt.plot([x for (x, y) in smoothedPath], [y for (x, y) in smoothedPath], '-c')
             plt.grid(True)
-    
-            numpy_path = np.asarray(path, dtype=np.float32)
-            x, y = zip(*numpy_path)
-            x = np.r_[x, x[0]]
-            y = np.r_[y, y[0]]
-            f, u = interpolate.splprep([x, y], s=0, per=True)
-            #create interpolated lists of points
-            xint, yint = interpolate.splev(np.linspace(0, 1, 100), f)
-            plt.scatter(x, y)
-            plt.plot(xint, yint)
-            plt.pause(0.0001)  # Need for Mac
+            #
+            # numpy_path = np.asarray(path, dtype=np.float32)
+            # x, y = zip(*numpy_path)
+            # x = np.r_[x, x[0]]
+            # y = np.r_[y, y[0]]
+            # f, u = interpolate.splprep([x, y], s=0, per=True)
+            # #create interpolated lists of points
+            # xint, yint = interpolate.splev(np.linspace(0, 1, 100), f)
+            # plt.scatter(x, y)
+            # # plt.plot(xint, yint)
+            # plt.pause(0.0001)  # Need for Mac
             plt.show()
-        
 
-       
         return path
 
-    def setMovement(self, yvel, xvel, yawrate):
-        self.sim.setJointTargetVelocity(self.front_left_wheel_joint, -yvel - xvel - yawrate)
-        self.sim.setJointTargetVelocity(self.rear_left_wheel_joint, -yvel + xvel - yawrate)
-        self.sim.setJointTargetVelocity(self.rear_right_wheel_joint, -yvel - xvel + yawrate)
-        self.sim.setJointTargetVelocity(self.front_right_wheel_joint, -yvel + xvel + yawrate)
+    def setMovement(self, velocity, yawrate):
+        robot_scaling_factor = self.sim.getObjectSizeFactor(self.robot_object)
+        R = 0.027 * robot_scaling_factor
+        h = 0.119 * robot_scaling_factor
+        self.sim.setJointTargetVelocity(self.right_wheel_joint, (velocity + yawrate * (h / 2) / R))
+        self.sim.setJointTargetVelocity(self.left_wheel_joint, (velocity - yawrate * (h / 2) / R))
 
     def getObjectPosition(self, object):
         return self.sim.getObjectPosition(object, self.world_frame)
@@ -145,7 +133,8 @@ class RobotControl:
         z = self.getObjectPosition(self.robot_object)[2]
         position = [x, y, z]
         orientation = self.getObjectOrientation(self.robot_object)
-        return self.setObjectPosition(self.robot_object, [x, y, z]) and self.setObjectOrientation(self.robot_object, orientation)
+        return self.setObjectPosition(self.robot_object, [x, y, z]) and self.setObjectOrientation(self.robot_object,
+                                                                                                  orientation)
 
     def startSimulation(self):
         # When simulation is not running, ZMQ message handling could be a bit
@@ -162,28 +151,24 @@ class RobotControl:
 
     def run(self):
         print('Program started')
-        #start simulation in CoppeliaSim
+        # start simulation in CoppeliaSim
         self.startSimulation()
-        print(self.rrt_path())
+        # print(self.getObjectOrientation(self.racks[0]))
+        # for i, rack in enumerate(self.racks):
+        #     print(self.getObjectOrientation(rack))
+        self.rrt_path()
         # while (t := self.sim.getSimulationTime()) < 15:
-        #     # self.setMovement(5, 0, 0)
-        #     self.sim.setJointTargetVelocity(self.joint_1, 0.01)
+        #     self.setMovement()
         #     self.client.step()
-        #
-        # for i in range(len(robot_velocities)):
-        #     self.setMovement(robot_velocities[i]*100, 0, robot_steering[i])
-        #     self.client.step()
-        
         self.stopSimulation()
         print('Program ended')
 
-if __name__ == '__main__':
 
-    control = RobotControl()
-    # path = control.rrt_path(RRT_star=True)
-    # pid = PID(np.array(path))
+if __name__ == '__main__':
+    # pid = PID(path)
     # pid.control()
     # robot_velocities = pid.velocity_path #array of vel inputs for robot
     # robot_steering = pid.steering_path #array of steering inputs for robot
-    #control.run(robot_velocities,robot_steering)
+
+    control = RobotControl()
     control.run()
